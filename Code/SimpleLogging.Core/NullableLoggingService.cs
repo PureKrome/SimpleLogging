199208d3172bddb44515e68944c9ee8b9037dba1
Pure Krome <github@world-domination.com.au>
2014-01-06 21:43:32 +1000

    Added a Nullable logging service (which doesn't log anything).

---
+++ Code/SimpleLogging.Core/NullableLoggingService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace SimpleLogging.Core
+{
+    public class NullableLoggingService : ILoggingService
+    {
+        public NullableLoggingService(string name = null)
+        {
+            Name = string.IsNullOrWhiteSpace(name)
+                ? "SimpleLogging.Core.NullableLoggingService"
+                : name;
+        }
+
+        public string Name { get; private set; }
+
+        public void Debug(string message)
+        {
+        }
+
+        public void Debug(string message, params object[] args)
+        {
+        }
+
+        public void Info(string message)
+        {
+        }
+
+        public void Info(string message, params object[] args)
+        {
+        }
+
+        public void Warning(string message)
+        {
+        }
+
+        public void Warning(string message, params object[] args)
+        {
+        }
+
+        public void Error(string message)
+        {
+        }
+
+        public void Error(string message, params object[] args)
+        {
+        }
+
+        public void Error(Exception exception, string message = null)
+        {
+        }
+
+        public void Fatal(string message)
+        {
+        }
+
+        public void Fatal(string message, params object[] args)
+        {
+        }
+
+        public void Fatal(Exception exception, string message = null)
+        {
+        }
+    }
+}
