2904f911b145b4aa56920abef1c0271002d1d98f
Pure Krome <github@world-domination.com.au>
2014-04-20 20:31:46 +1000

    Updated version and nuget packages to v 0.4.0.

--- Code/Shared Assembly Info/SharedAssemblyInfo.cs
+++ Code/Shared Assembly Info/SharedAssemblyInfo.cs
@@ -23,5 +23,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
